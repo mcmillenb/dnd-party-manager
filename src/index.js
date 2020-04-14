b0d1d7006d60bb8ff581c020e56d9b3d9b67ba40
Brian McMillen <brian.mcmillen@explorelearning.com>
2020-04-13 23:24:01 -0400

    use axios and cheerio to load and parse the data from the dnd beyond url

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -11,4 +13,24 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+const characterUrl = 'https://www.dndbeyond.com/profile/brianmcmillen1/characters/6626114';
+axios({
+  url: characterUrl,
+  method: 'get',
+  withCredentials: true,
+  headers: {
+    'Cache-Control': 'no-cache',
+    'Cookies': 'foo=bar',
+    'User-Agent': 'dnd-party-manager',
+    'Accept': '*/*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive'
+  }
+}).then((resp) => {
+  const $ = cheerio.load(resp.data);
+  console.log('Title:', $('h1.page-title').text().trim());
+}).catch((error) => {
+  console.error(error);
 });
